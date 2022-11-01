1c3a3d1cce5dc019057e1e89e28dde1635fb9a83
Adrian Bishop <adrianbishopemail@gmail.com>
2022-11-01 11:23:29 +0000

    Initial commit - adding dependencies for Dynamo, barebones serverless yaml and the initial dynamo scripts required for adding a new account to the database

---
+++ ddbClient.js
@@ -0,0 +1,26 @@
+// create2.js #
+
+//To prevent a new item from replacing an existing item, use a conditional
+//expression that contains the <code>attribute_not_exists</code>function with the
+// name of the attribute being used as the partition key for the table
+
+//AWS REF https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/example_dynamodb_PutItem_section.html
+
+'use strict';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+export const REGION = "eu-west-1"
+export const ddbClient = DynamoDBClient({ region: REGION })
+
+import { v4 as uuidv4, __esModule } from 'uuid';
+
+const newAccount = {
+  id: uuidv4(),
+  name: `Account Name`
+  };
+
+const putNewAccount = () => {
+  DynamoDBClient(putItem(newAccount))
+};
+
+
+module.exports = { putNewAccount }
---
+++ setup_notes.md
@@ -0,0 +1,22 @@
+### README ###
+
+# Install Node 16 #
+`curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&sudo apt-get install -y nodejs`
+
+# Install n #
+`sudo npm i -g n`
+
+# Change node Version #
+`sudo n`
+
+# npm Install # 
+# (Updated the uuid version in package.json)
+- Change node version to 16.x to match with lambda
+`npm install`
+
+
+# AWS SDK v3 is out and it's modular
+# https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-dynamodb/index.html
+# import { DynamoDBClient, BatchExecuteStatementCommand } from "@aws-sdk/client-dynamodb";
+`npm install @aws-sdk/client-dynamodb`
+`npm add @aws-sdk/lib-dynamodb`
